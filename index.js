997a4c58be0e805726db3ab9c0f7acfc084df633
neev493 <110727962+neev493@users.noreply.github.com>
2024-01-15 00:04:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomImg1 = "images/dice"+randomNumber1+".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImg1);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomImg2 = "images/dice"+randomNumber2+".png";
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImg2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!"
+}else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩"
+}else{
+    document.querySelector("h1").innerHTML = "Drawww!😶"
+}
